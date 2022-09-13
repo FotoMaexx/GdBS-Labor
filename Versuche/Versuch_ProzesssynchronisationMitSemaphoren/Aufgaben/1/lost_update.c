f5dea1b7f2d7515e3b1bd80aae703395d97c97c6
Maximilian Hauser <info@hauser-media.com>
2022-09-13 16:36:31 +0200

    Added solutions for Versuch Prozesssynchronisation mit Semaphoren: Aufgabe 1

---
+++ Versuche/Versuch_ProzesssynchronisationMitSemaphoren/Aufgaben/1/lost_update.c
@@ -0,0 +1,70 @@
+#include "workers.h"
+#include "semaphores.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define A_BIG_NUMBER 500000
+#define WORKERS 3
+
+//-----------------------------------------------------------------------------
+// alle globalen variablen fuer die beiden worker hier definieren,
+// alle unbedingt mit "volatile" !!!
+//-----------------------------------------------------------------------------
+
+volatile int global_var=0;
+
+// semaphore deklariert man hier z.B. wie folgt:
+  semaphore mein_semaphor;
+
+//-----------------------------------------------------------------------------
+// bevor der test beginnt wird test_setup() einmal aufgerufen
+// - die variablen  readers  bzw.  writers  muessen gesetzt werden: wieviele
+//   prozesse sollen parallel die funktionen reader bzw. writer bearbeiten?
+//-----------------------------------------------------------------------------
+
+void test_setup(void) {
+  printf("Test Setup\n");
+  global_var=0;
+  readers=0;
+  writers=WORKERS;
+  // initialisieren von sempahoren hier z.B. wie folgt:
+    mein_semaphor = sem_init(1);
+}
+
+//-----------------------------------------------------------------------------
+// wenn beider worker fertig sind wird test_end() noch aufgerufen
+//-----------------------------------------------------------------------------
+
+void test_end(void) {
+  printf("Test End\n");
+  int expected = WORKERS * A_BIG_NUMBER;
+  printf("global_var is %i, should be %i\n", global_var, expected);
+  if (global_var != expected) {
+    printf("'Lost update' failed\n");
+  } else {
+    printf("'Lost update' ok\n");
+  }
+}
+
+//-----------------------------------------------------------------------------
+// die beiden worker laufen parallel:
+//-----------------------------------------------------------------------------
+
+void reader(long my_id) {
+  printf("Wer hat mich da aufgerufen? Nicht gut!\n");
+  exit(1);
+}
+
+// im writer semaphore-operationen einbauen, also so was wie:
+
+void writer(long my_id) {
+  sem_p(mein_semaphor);
+  int i;
+  for (i=0; i<A_BIG_NUMBER; i++) {
+    for (int x=0; x<888; x++); // "lange Rechnung"
+    global_var += 1;
+    // printf("Worker1: %i\n", global_var);
+  }
+  sem_v(mein_semaphor);
+}
---
+++ Versuche/Versuch_ProzesssynchronisationMitSemaphoren/Aufgaben/1/solution.md
@@ -0,0 +1,5 @@
+# Lösungen Aufgabe 1
+
+## warum funktioniert das nicht korrekt?
+
+Da die writer gleichzeitig schreiben und somit den global_var überschreiben, während ein anderer writer gerade auf diesen schreibt. 
